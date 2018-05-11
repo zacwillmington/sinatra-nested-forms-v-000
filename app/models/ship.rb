2d5065aaa848eb1cb3cfc9eab2bc7b08a510ba9f
Zac Willmington <zac.willmington@gmail.com>
2018-05-11 19:24:40 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(name, type, booty)
+        @name  = name
+        @type = type
+        @booty = booty
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
