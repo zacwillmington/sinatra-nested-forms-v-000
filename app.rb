aa6104c686f81a2ef4b66e258c599b7a05702964
Zac Willmington <zac.willmington@gmail.com>
2018-05-11 19:37:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,17 +3,15 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
-
-        
+    
         get '/' do
             erb :index
         end
-        
+
         get '/new' do
             erb :new
         end
-        
+
         post '/show' do
             erb :show
         end
