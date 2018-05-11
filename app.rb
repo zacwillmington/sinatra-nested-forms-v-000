f306d74b94ed33e55e58c97dec994f55fe288188
Zac Willmington <zac.willmington@gmail.com>
2018-05-11 20:19:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,10 @@ module FormsLab
 
         post '/show' do
             @pirate = Priate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height] )
-            
-            @ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty] )
+
+            @ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
+
+            @ship2 = Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
 
 
             erb :'pirates/show'
